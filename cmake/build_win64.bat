efea22143af087ac7576a2ea8c029721037733ee
hongy <hongy3025@163.com>
2021-05-10 21:18:44 +0800

    cmake for win64

--- cmake/build_win64.bat
+++ cmake/build_win64.bat
@@ -1,8 +1,2 @@
-if not exist build.win64 mkdir build.win64
-
-pushd build.win64
-
-cmake -G "Visual Studio 16 2019" -A x64 ..
-popd
-
+cmake -B build.win64 -G "Visual Studio 16 2019" -A x64
 cmake --build build.win64 --config Release
